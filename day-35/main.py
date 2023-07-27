1d12b6f9a50268c5902739a8aea2453e2adf563e
Joey Don <joeydon21@gmail.com>
2023-07-26 21:26:57 -0400

    updated code on day-35 to send text message if it snows as well.

--- day-35/main.py
+++ day-35/main.py
@@ -3,7 +3,7 @@ import os
 from twilio.rest import Client
 
 weather_api = "https://api.openweathermap.org/data/2.8/onecall"
-api_key = os.environ.get("OWM_API_KEY")
+api_key = os.environ.get("API_KEY")
 MY_LAT = 42.083302
 MY_LONG = -71.019897
 account_sid = os.environ.get("TWILIO_ACC_SID")
@@ -24,18 +24,29 @@ weather_data = response.json()
 hourly_weather = (weather_data["hourly"][:12])
 
 will_rain = False
+will_snow = False
 
 for hour_data in hourly_weather:
     condition_code = hour_data["weather"][0]["id"]
-    if int(condition_code) < 700:
+    if int(condition_code) < 600:
         will_rain = True
+    elif 500 < int(condition_code) < 700:
+        will_snow = True
+
 
 client = Client(account_sid, auth_token)
 if will_rain:
     message = client.messages.create(body="It's going to rain today. Bring umbrella!",
                                      from_='+18665083998', to='+17742402535')
-    print(message.sid)
 else:
     message = client.messages.create(body="No rain today. You good!",
                                      from_='+18665083998', to='+17742402535')
-    print(message.sid)
+print(message.sid)
+
+if will_snow:
+    message = client.messages.create(body="It's going to snow today. Call out!",
+                                     from_='+18665083998', to='+17742402535')
+else:
+    message = client.messages.create(body="No snow you good!",
+                                     from_='+18665083998', to='+17742402535')
+print(message.sid)
