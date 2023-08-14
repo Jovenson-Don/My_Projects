51f098a5d1590c498452424bece1b530b7284899
Joey Don <joeydon21@gmail.com>
2023-08-14 19:38:08 -0400

    completed day 46

--- day-46/main.py
+++ day-46/main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+import os
+import bs4
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+URL = "https://www.billboard.com/charts/hot-100/"
 
+SPOTIFY_REDIRECT_URI = "http://example.com"
+SPOTIFY_ID = os.environ.get("spotify_id")
+SPOTIFY_SECRET = os.environ.get("spotify_secret")
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_ID,
+                                               client_secret=SPOTIFY_SECRET,
+                                               redirect_uri=SPOTIFY_REDIRECT_URI,
+                                               scope="playlist-modify-private",
+                                               show_dialog=True,
+                                               cache_path="token.txt",
+                                               username="jovensondon")
+                     )
+user_id = sp.current_user()["id"]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 
+response = requests.get(URL + date)
+website = response.text
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+soup = bs4.BeautifulSoup(website, "html.parser")
+song_titles = soup.find_all(name="h3", class_="a-no-trucate")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+song_title = [title.get_text().strip() for title in song_titles]
+
+playlist = sp.user_playlist_create(user=user_id,
+                                   name=f"{date} Billboard 100",
+                                   public=False,
+                                   description=f"Top 100 songs on {date}")
+
+song_url = []
+year = date.split("-")[0]
+for song in song_title:
+    results = sp.search(q=f"track:{song} year:{year}", type="track")
+    try:
+        url = results["tracks"]["items"][0]["external_urls"]["spotify"]
+        song_url.append(url)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_url)
