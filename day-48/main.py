d2692e15a597b3e4c11cc97334f6c8b7b8cd09ff
Joey Don <joeydon21@gmail.com>
2023-08-18 16:06:00 -0400

    day 48 updated

--- day-48/main.py
+++ day-48/main.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -9,9 +10,15 @@ options.add_experimental_option(name="detach", value=True)
 driver = webdriver.Chrome(service=Service(), options=options)
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
 
-language = driver.find_element(By.ID, "promptAnchor")
-print(language.text)
-language.click()
+cookie = driver.find_element(By.ID, "cookie")
 
-cookie = driver.find_element(By.ID, "cookies")
-cookie.click()
+cookie_counter = 0
+while time.time():
+    cookie.click()
+    cookie_counter += 1
+
+
+
+
+
+
