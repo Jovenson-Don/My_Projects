997355c5f5aa2b9ef46db04d97549c81b5d8a1b5
Joey Don <joeydon21@gmail.com>
2023-12-15 15:12:57 -0500

    finished terraform challenge

--- terraform_challange/main.tf
+++ terraform_challange/main.tf
@@ -13,10 +13,20 @@ resource "docker_container" "php-httpd" {
   name = "webserver"
   hostname = "php-httpd"
   image = docker_image.php-httpd-image.name
-  network_alias =  ["my_network"]
+  networks_advanced {
+    name = docker_network.private_network.name
+  }
   ports {
     internal = 80
     external = 80
     ip = "0.0.0.0"
+  }
+  labels {
+    label = "challenge"
+    value = "second"
+  }
+  volumes {
+    host_path = "/root/code/terraform-challenges/challenge2/lamp_stack/website_content/"
+    container_path = "/var/www/html"
   }
 }
