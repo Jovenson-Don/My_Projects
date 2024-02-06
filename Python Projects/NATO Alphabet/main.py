62997012eede0adaf7dfa90349f3fe1ecaf1936a
Joey Don <joeydon21@gmail.com>
2024-02-06 11:08:29 -0500

    Updating NATO python code

--- Python Projects/NATO Alphabet/main.py
+++ Python Projects/NATO Alphabet/main.py
@@ -1,35 +1,9 @@
-# student_dict = {
-#     "student": ["Angela", "James", "Lily"],
-#     "score": [56, 76, 98]
-# }
-#
-# #Looping through dictionaries:
-# for (key, value) in student_dict.items():
-#     #Access key and value
-#     pass
-#
-# import pandas
-# student_data_frame = pandas.DataFrame(student_dict)
-#
-# #Loop through rows of a data frame
-# for (index, row) in student_data_frame.iterrows():
-#     #Access index and row
-#     #Access row.student or row.score
-#     pass
-#
-# # Keyword Method with iterrows()
-# # {new_key:new_value for (index, row) in df.iterrows()}
-#
-# #TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-#
-# # #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 import pandas
 
 nato_dict = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_phonetic = {row.letter: row.code for (index, row) in nato_dict.iterrows()}
 
-print(nato_dict)
+print(nato_phonetic)
 def restart_phonetic():
     word = input("Enter a word: ").upper()
     try:
@@ -39,4 +13,6 @@ def restart_phonetic():
         restart_phonetic()
     else:
         print(results)
+
+
 restart_phonetic()
