c2833edaa376eb36f90fc2a56f2711b3c4cae44b
Joey Don <joeydon21@gmail.com>
2024-02-27 17:52:09 -0500

    Updates to nba projects

--- NBA Stat Project/app.py
+++ NBA Stat Project/app.py
@@ -5,30 +5,35 @@ from datetime import datetime
 app = Flask("app")
 
 api = "https://nba-stats-db.herokuapp.com/api/playerdata/name/"
+date = datetime.now().year - 1
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route("/stats", methods=["POST", "GET"])
+@app.route("/results", methods=["POST", "GET"])
 def receive_stats():
     player = request.form["player"]
-    year = request.form["year"]
-    data = requests.get(api + player).json()
-    results = data["results"][0]
-    player_full_name = results["player_name"]
-    ppg = float(format(results["PTS"] / results["games"], ".1f"))
-    apg = float(format(results["AST"] / results["games"], ".1f"))
-    rpg = float(format(results["TRB"] / results["games"], ".1f"))
-    spg = float(format(results["STL"] / results["games"], ".1f"))
-    bpg = float(format(results["BLK"] / results["games"], ".1f"))
-    tpg = float(format(results["TOV"] / results["games"], ".1f"))
-    fgp = format(float(results["field_percent"]) * 100, ".1f")
-    return (f"<h1>{player_full_name} averages for {year} season: {ppg}PPG, {apg}APG, "
-            f"{rpg}RPG, {spg}SPG, {bpg}BLK, {tpg}TOV {fgp}FG</h1>")
-
-
+    year = int(request.form["year"])
+    try:
+        data = requests.get(api + player).json()
+        results = data["results"]
+        if date-year < 0 or date-year > data["count"]:
+            return "<h1> No stats for that season or season didn't happen yet</h1>"
+        else:
+            ppg = float(format(results[date-year]["PTS"] / results[date-year]["games"], ".1f"))
+            apg = float(format(results[date-year]["AST"] / results[date-year]["games"], ".1f"))
+            rpg = float(format(results[date-year]["TRB"] / results[date-year]["games"], ".1f"))
+            spg = float(format(results[date-year]["STL"] / results[date-year]["games"], ".1f"))
+            bpg = float(format(results[date-year]["BLK"] / results[date-year]["games"], ".1f"))
+            tpg = float(format(results[date-year]["TOV"] / results[date-year]["games"], ".1f"))
+            fgp = format(float(results[date-year]["field_percent"]) * 100, ".1f")
+            return (f"<h1>{player} averages for the {year} season: {ppg}PPG, {apg}APG, "
+                    f"{rpg}RPG, {spg}SPG, {bpg}BLK, {tpg}TOV {fgp}FG</h1>")
+    except IndexError:
+        return f"<h1>{player} never played in nba</h1>".title()
 
 
 app.run(debug=False)
