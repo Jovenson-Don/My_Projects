acbd1ed17af79f06ea82dac1f1189cce69bae3c9
Joey Don <joeydon21@gmail.com>
2024-12-19 16:19:36 -0500

    updated flask server code

--- NBA Stat Project/app.py
+++ NBA Stat Project/app.py
@@ -12,29 +12,32 @@ def home():
 
 @app.route("/results", methods=["POST", "GET"])
 def receive_stats():
-    api = "https://nba-stats-db.herokuapp.com/api/playerdata/name/"
-    date = datetime.now().year - 1
+    api = "http://b8c40s8.143.198.70.30.sslip.io/api/PlayerDataTotals/name/"
+    date = datetime.now().year
     try:
         player = request.form["player"].strip()
         year = int(request.form["year"])
         data = requests.get(api + player).json()
-        results = data["results"]
-        player_full_name = results[0]["player_name"]
-        if date - year < 0 or date - year > data["count"]:
-            player_results = (f"{player_full_name} didn't play in that season or the season "
-                              f"hasn't happened yet.")
-            return render_template("results.html", results=player_results)
-        else:
-            ppg = float(format(results[date - year]["PTS"] / results[date - year]["games"], ".1f"))
-            apg = float(format(results[date - year]["AST"] / results[date - year]["games"], ".1f"))
-            rpg = float(format(results[date - year]["TRB"] / results[date - year]["games"], ".1f"))
-            spg = float(format(results[date - year]["STL"] / results[date - year]["games"], ".1f"))
-            bpg = float(format(results[date - year]["BLK"] / results[date - year]["games"], ".1f"))
-            tpg = float(format(results[date - year]["TOV"] / results[date - year]["games"], ".1f"))
-            fgp = format(float(results[date - year]["field_percent"]) * 100, ".1f")
-            player_info = f"{player_full_name} averages for the {year} season"
-            player_results = f"{ppg}PPG {apg}APG {rpg}RPG {spg}SPG {bpg}BLK {tpg}TOV {fgp}FG"
-            return render_template("results.html", results=player_results, results_info=player_info)
+        player_full_name = data[0]["playerName"]
+        for i in data:
+            if i["season"] == year:
+                ppg = float(format(i["points"] / i["games"], ".1f"))
+                apg = float(format(i["assists"] / i["games"], ".1f"))
+                rpg = float(format(i["totalRb"] / i["games"], ".1f"))
+                spg = float(format(i["steals"] / i["games"], ".1f"))
+                bpg = float(format(i["blocks"] / i["games"], ".1f"))
+                tpg = float(format(i["turnovers"] / i["games"], ".1f"))
+                fgp = format(float(i["fieldPercent"]) * 100, ".1f")
+                tpgp = format(float(i["threePercent"]) * 100, ".1f")
+                ft = format(float(i["ftPercent"]) * 100, ".1f")
+
+                player_info = f"{player_full_name} averages for the {year} season"
+                player_results = f"{ppg}PPG {apg}APG {rpg}RPG {spg}SPG {bpg}BLK {tpg}TOV {fgp}FG {tpgp}3FG {ft}FT"
+                return render_template("results.html", results=player_results, results_info=player_info)
+            # else:
+            #     player_results = (f"{player_full_name} didn't play in that season or the season "
+            #                       f"hasn't happened yet.")
+            #     return render_template("results.html", results=player_results)
     except ValueError:
         return render_template("index.html")
     except IndexError:
