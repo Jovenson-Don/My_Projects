75ea733f750435367ba6e2eab057838e42933a2d
Joey Don <joeydon21@gmail.com>
2025-03-24 10:58:40 -0400

    Day 5 of python coding

--- Python Projects/Day_5-Password Generator/main.py
+++ Python Projects/Day_5-Password Generator/main.py
@@ -9,11 +9,15 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-password = ""
+password = []
 for n in range(0, nr_letters):
     password += random.choice(letters)
 for n in range(0, nr_symbols):
     password += random.choice(symbols)
 for n in range(0, nr_numbers):
     password += random.choice(numbers)
-print(password)
+random.shuffle(password)
+print("".join(password))
+
+
+
