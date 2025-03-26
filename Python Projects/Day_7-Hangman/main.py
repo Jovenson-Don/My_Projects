901c448bf7feb016e8b80cbaea6d7901bd3292b0
Joey Don <joeydon21@gmail.com>
2025-03-26 13:16:26 -0400

    Organized hangman projects

--- Python Projects/Day_7-Hangman/main.py
+++ Python Projects/Day_7-Hangman/main.py
@@ -2,48 +2,50 @@ import random
 import hangman_words
 from hangman_art import logo, stages
 
+# Initialize variables
 words = hangman_words.word_list
 chosen_word = random.choice(words)
 word_length = len(chosen_word)
-end_of_game = False
 lives = 6
-
-print(logo)
-# Create blanks
-display = []
-for _ in range(word_length):
-    display += "_"
+end_of_game = False
 guessed_letters = []
+
+# Display logo
+print(logo)
+
+# Create blanks
+display = ["_"] * word_length
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-    if guess in display:
-        print(f"You already guessed this {guess}")
+    # Check if letter was already guessed
+    if guess in guessed_letters:
+        print(f"You've already guessed '{guess}'")
+        continue
 
-    # Check guessed letter
-    for position in range(word_length):
-        letter = chosen_word[position]
-        if letter == guess:
-            display[position] = letter
-            print(display)
+    # Add guess to guessed letters
+    guessed_letters.append(guess)
 
-    if guess not in guessed_letters:
-        guessed_letters += guess
-
-    if guess not in chosen_word:
-        print(f"{guess} is not in this word")
+    # Check if guess is in the word
+    if guess in chosen_word:
+        for position in range(word_length):
+            if chosen_word[position] == guess:
+                display[position] = guess
+        print(f"Good guess! {' '.join(display)}")
+    else:
+        print(f"'{guess}' is not in the word.")
         lives -= 1
         if lives == 0:
             end_of_game = True
-            print("You lose.The chosen word was " + chosen_word)
-        # Join all the elements in the list and turn it into a String.
+            print(f"You lose. The chosen word was '{chosen_word}'")
+            break
 
-    print(f"{''.join(display)}")
-
+    # Check if the player has guessed all the letters
     if "_" not in display:
         end_of_game = True
-        print("You win.")
+        print("Congratulations! You win!")
 
+    # Show remaining lives and guessed letters
     print(stages[lives])
-    print(f"These are the letters you guessed so far {guessed_letters}")
+    print(f"Guessed letters: {', '.join(guessed_letters)}")
