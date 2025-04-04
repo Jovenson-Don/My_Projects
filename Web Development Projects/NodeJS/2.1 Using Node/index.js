ddba6537b51d8a73e7bc035dbb4e0715655ce0dc
Joey Don <joeydon21@gmail.com>
2025-04-04 17:15:04 -0400

    Learning node JS

---
+++ Web Development Projects/NodeJS/2.1 Using Node/index.js
@@ -0,0 +1,2 @@
+console.log("Hello World");
+let name = prompt("hello, whats your name? ")
---
+++ Web Development Projects/NodeJS/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`)
+
+import randomSuperhero from "superheroes";
+const name = randomSuperhero();
+
+console.log(`My superhero name is ${name}`)
