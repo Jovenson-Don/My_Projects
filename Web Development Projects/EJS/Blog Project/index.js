57042e021e192e145b58e7dae0fa5a7a51dad789
Joey Don <joeydon21@gmail.com>
2025-04-18 17:18:15 -0400

    Starting blog project

--- Web Development Projects/EJS/Blog Project/index.js
+++ Web Development Projects/EJS/Blog Project/index.js
@@ -1,10 +1,26 @@
 import express from "express";
-import bodyParser from "bodyParser"
+import bodyParser from "body-parser";
+
 
 const app = express();
 const port = "3000";
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
-    res.send("hello");
+    res.render("index.ejs");
+});
+
+app.get("/create", (req, res) => {
+    res.render("create.ejs")
+})
+
+app.get("/viewblogs", (req, res) => {
+    res.render("create.ejs")
+})
+
+
+app.listen(port, () => {
+    console.log(`Listen on port: ${port}`)
 });
