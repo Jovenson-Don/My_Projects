0d990850bef192748b54c99fcc63653592eb6a81
Joey Don <joeydon21@gmail.com>
2025-04-19 16:16:51 -0400

    Updated Blog project

--- Web Development Projects/EJS/Blog Project/index.js
+++ Web Development Projects/EJS/Blog Project/index.js
@@ -5,34 +5,44 @@ import bodyParser from "body-parser";
 const app = express();
 const port = "3000";
 const allBlogs = []
+let blogID = 0
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"))
 
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {blogs: allBlogs});
 });
 
 app.get("/create", (req, res) => {
     res.render("create.ejs");
 });
 
-app.get("/viewblogs", (req, res) => {
-    res.render("viewblogs.ejs", {blogs: allBlogs});
-});
-app.get("/view", (req, res) => {
-    res.render("view.ejs");
-    console.log(req.body);
+app.get("/all-blogs", (req, res) => {
+    res.render("all-blogs.ejs", {blogs: allBlogs});
 });
 
-app.post("/createdblog", (req, res) => {
-    const results = req.body;
-    allBlogs.push(results);
-    console.log(req.body);
-    res.render("createdblog.ejs", {data: results});
+app.get("/edit-blog/:id", (req, res) => {
+    blogID = req.params.id;
+    res.render("edit-blog.ejs", {post: allBlogs[blogID]});
+});
+app.post("/submit", (req,res) => {
+    allBlogs.push(req.body);
+    res.redirect("/all-blogs");
 });
 
+app.post("/updated", (req, res) => {
+    allBlogs[blogID] = req.body;
+    res.redirect("/all-blogs");
+    
+});
+
+app.get("/delete/:id", (req, res) => {
+    blogID = req.params.id;
+    allBlogs.pop(blogID);
+    res.redirect("/all-blogs");
+});
 
 app.listen(port, () => {
     console.log(`Listen on port: http://127.0.0.1:${port}`)
