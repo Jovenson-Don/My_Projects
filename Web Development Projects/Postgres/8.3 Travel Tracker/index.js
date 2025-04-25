78432b682e4f35e128a7912217d949193e907df0
Joey Don <joeydon21@gmail.com>
2025-04-25 16:41:16 -0400

    More DB practice

---
+++ Web Development Projects/Postgres/8.3 Travel Tracker/index.js
@@ -0,0 +1,50 @@
+import express, { response } from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "Gameover21",
+  port: 5432
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  //Write your code here.
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM visited_countries"
+    );
+    const countryCodes = result.rows.map(item => item.country_code);
+    res.render("index.ejs", { countries: countryCodes, total: result.rows.length});
+    db.end();
+  } catch (error) {
+    console.error("Error:", error)
+    res.render("index.ejs", {error: error})
+  }
+});
+
+app.post("/add", async (req, res) => {
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1", 
+    [req.body.country]);
+
+  const countryCode = result.rows[0].country_code;
+
+  await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", 
+    [countryCode]);
+
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
