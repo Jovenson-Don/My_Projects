5d37552a893021c5d710cee71236ab086537166f
Joey Don <joeydon21@gmail.com>
2025-05-09 17:10:44 -0400

    Section 3 of react

---
+++ Web Development Projects/React/react-projects/src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return (
+        <header>
+            <div className="header-box">
+                <img src="./chef-claude-icon.png" alt="Chef Claude icon" />
+                <span>Chef Claude</span>
+            </div>
+        </header>
+    )
+    
+}
