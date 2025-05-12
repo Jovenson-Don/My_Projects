d54cddfc0b8c971ff472d95c9a5f166bd4f17ea7
Joey Don <joeydon21@gmail.com>
2025-05-12 17:00:31 -0400

    More react practice

--- Web Development Projects/React/react-projects/src/App.jsx
+++ Web Development Projects/React/react-projects/src/App.jsx
@@ -1,7 +1,11 @@
 import Header from "./components/Header"
+import Main from "./components/Main"
 
 export default function App() {
   return (
+    <>
       <Header />
+      <Main />
+    </>
   )
 }
---
+++ Web Development Projects/React/react-projects/src/components/Main.jsx
@@ -0,0 +1,35 @@
+import {useState} from "react"
+export default function Main() {n  
+
+    const [ingredients, setIngredients] = useState([])
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prev => [...prev, newIngredient])
+    }
+
+    return (
+        <main>
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
+        </main>
+    )
+}
